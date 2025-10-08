cb06804867e72c52289dff2e5638cc3e1eaff34c
Richard Kooijman <kooijman.richard@gmail.com>
2025-10-08 15:32:21 +0200

    fix op personen PATH

--- src/personen.py
+++ src/personen.py
@@ -24,8 +24,6 @@ PERSONEN_TEST_BSN = "999972030"
 PERSONEN_TEST_POSTCODE = "1014CB"
 PERSONEN_TEST_HUISNUMMER = "20"
 
-PERSONEN_PATH = "/haalcentraal/api/brp/personen"
-
 TOKEN = Token(os.environ.get("INT_TEST_TOKEN", "int-test-token"))
 ROLES = TOKEN.roles
 
